40b79d2f51cf327bfee1ae681e490f5005ff324b
Kevin Doyle <kdoyle2@gmail.com>
2012-02-02 14:25:44 -0500

    allow for multiple markers when specifying latlng

--- jquery.googleMap.js
+++ jquery.googleMap.js
@@ -10,7 +10,7 @@
 	$.fn.googleMap = function(options) {
 		var defaults = {
 			address: false,
-			LatLng: [0, 0],
+			LatLng: [[0, 0]],			
 			zoom: 8, //higher number means zoom in further
 			icon: false,
 			alt: false,
@@ -19,9 +19,9 @@
 		}
 		var options = $.extend(defaults, options);
 		return this.each(function() {
-			var obj = $(this)[0];
-			var mapLatLng = new google.maps.LatLng(options.LatLng[0],options.LatLng[1]);
-			var mapTypeDisplay;
+			var obj = $(this)[0],
+				mapLatLng = new google.maps.LatLng(options.LatLng[0][0],options.LatLng[0][1]),
+				mapTypeDisplay, mapOptions, map, geocoder, bounds, tempLatLng, tempMarker, tempLat, tempLng;
 
 			switch(options.mapType){
 				case "roadmap":
@@ -38,46 +38,40 @@
 					break;
 			}
 			
-			var mapOptions = {
-				zoom: options.zoom,
+			mapOptions = {
+				zoom: (options.zoom === "auto")?(0):(options.zoom),
 				center: mapLatLng,
 				mapTypeId:mapTypeDisplay
-				
 			}
-			var map = new google.maps.Map(obj,mapOptions);
+			
+			bounds = new google.maps.LatLngBounds();
+			map = new google.maps.Map(obj,mapOptions);
 			if(options.address){
 				var geocoder = new google.maps.Geocoder();	
-				geocoder.geocode( { 'address': options.address}, function(results, status) {
-					if (status == google.maps.GeocoderStatus.OK) {
+				geocoder.geocode({'address': options.address}, function(results, status) {
+					if (status === google.maps.GeocoderStatus.OK) {
 						map.setCenter(results[0].geometry.location);
-						if(options.icon){
-							var marker = new google.maps.Marker({
-								map: map, 
-								position: results[0].geometry.location,
-								icon:options.icon
-							})
-						} else {
-							var marker = new google.maps.Marker({
-								map: map, 
-								position: results[0].geometry.location
-							});
-						}
-					} else if(status == google.maps.GeocoderStatus.ZERO_RESULTS) {
-						defaults.onGeocodeError.call(this);
+						var marker = new google.maps.Marker({
+							map: map, 
+							position: results[0].geometry.location,
+							icon: (options.icon)?(options.icon):(null)
+						});
+					} else if(status === google.maps.GeocoderStatus.ZERO_RESULTS) {
+						options.onGeocodeError.call(this);
 					}
 				});
 			} else {
-				if(options.icon){
-					var marker = new google.maps.Marker({
-						map: map, 
-						position: mapLatLng,
-						icon:options.icon
-					})
-				} else {
-					var marker = new google.maps.Marker({
-						map: map, 
-						position: mapLatLng
+				for (var i=0; i<=options.LatLng.length-1; i++){
+					tempLatLng = new google.maps.LatLng(options.LatLng[i][0],options.LatLng[i][1]);
+					tempMarker = new google.maps.Marker({
+						map: map,
+						position: tempLatLng,
+						icon: (options.icon)?(options.icon):(null)
 					});
+					bounds.extend(tempMarker.getPosition());
+				}
+				if(options.zoom === "auto"){
+					map.fitBounds(bounds);
 				}
 			}
 		});
